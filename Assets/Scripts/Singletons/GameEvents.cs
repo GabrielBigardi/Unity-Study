15937dee5ae586a31df4fd26e0f1896f5bffeb98
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2022-06-26 12:36:55 -0300

    Update GameEvents.cs

--- Assets/Scripts/Singletons/GameEvents.cs
+++ Assets/Scripts/Singletons/GameEvents.cs
@@ -7,6 +7,5 @@ public class GameEvents : MonoBehaviour
 {
     public static Action<Vector2> PlayerMoveStartEvent;
     public static Action<Vector2> PlayerMoveEndEvent;
-
     public static Action PlayerCollectedItem;
 }
