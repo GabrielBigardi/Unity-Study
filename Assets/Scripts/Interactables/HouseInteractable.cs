832ab5f532754ede0f9821eb3ecb0eac4a180002
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2022-06-29 22:29:42 -0300

    Changes

--- Assets/Scripts/Interactables/HouseInteractable.cs
+++ Assets/Scripts/Interactables/HouseInteractable.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class HouseInteractable : MonoBehaviour, IInteractable
 {
+	public bool CanBeInteracted = true;
 	public GameObject interactionSprite;
 
 	public void DisableInteraction()
@@ -13,12 +14,18 @@ public class HouseInteractable : MonoBehaviour, IInteractable
 
 	public void EnableInteraction()
 	{
+		if (!CanBeInteracted) return;
+
 		GameEvents.PlayerCloseToInteractable?.Invoke(this);
 		interactionSprite.SetActive(true);
 	}
 
 	public void Interact()
 	{
+		if (!CanBeInteracted) return;
+
+		DisableInteraction();
 		GameEvents.PlayerInteractionCompleted?.Invoke(this);
+		CanBeInteracted = false;
 	}
 }
--- Assets/Scripts/Singletons/UIManager.cs
+++ Assets/Scripts/Singletons/UIManager.cs
@@ -2,16 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-	[SerializeField] private GameObject lifeText;
+	[SerializeField] private GameObject _lifeText;
+	[SerializeField] private Image _fadeImage;
 
-    private void OnEnable() => GameEvents.PlayerLifeChanged += RefreshLifeText;
-    private void OnDisable() => GameEvents.PlayerLifeChanged -= RefreshLifeText;
+	private void OnEnable()
+    {
+		GameEvents.PlayerLifeChanged += RefreshLifeText;
+		GameEvents.PlayerInteractionCompleted += HandlePlayerInteractions;
+	}
+
+    private void OnDisable()
+    {
+		GameEvents.PlayerLifeChanged -= RefreshLifeText;
+		GameEvents.PlayerInteractionCompleted -= HandlePlayerInteractions;
+	}
+
+    private void Start()
+    {
+		_fadeImage.color = Color.black;
+		_fadeImage.DOColor(Color.clear, 1f);
+
+	}
 
     public void RefreshLifeText(int newLifes)
 	{
-		lifeText.SetTextOutline(newLifes.ToString());
+		_lifeText.SetTextOutline(newLifes.ToString());
 	}
+
+	private void HandlePlayerInteractions(IInteractable interactable)
+	{
+		if (interactable is HouseInteractable houseInteractable)
+			EnterHouse();
+	}
+
+	public void EnterHouse()
+    {
+		var activeScene = SceneManager.GetActiveScene().buildIndex;
+		_fadeImage.DOColor(Color.black, 1f).OnComplete(() => _fadeImage.DOColor(Color.clear, 1f));
+    }
 }
