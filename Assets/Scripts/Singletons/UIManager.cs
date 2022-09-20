013bfad0837dc20e000648202663df60c52f3b8b
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2022-09-20 18:12:55 -0300

    Removed unused debug

--- Assets/Scripts/Singletons/UIManager.cs
+++ Assets/Scripts/Singletons/UIManager.cs
@@ -38,7 +38,7 @@ public class UIManager : MonoBehaviour
 		_fadeImage.color = Color.black;
 		_fadeImage.DOColor(Color.clear, 1f);
 
-		Debug.Log(new Color().FromHex("#ff0000"));
+		//Debug.Log(new Color().FromHex("#ff0000"));
 	}
 
 	public void RefreshLifeText(int newLifes)
